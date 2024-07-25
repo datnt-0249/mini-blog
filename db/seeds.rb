01b9d97403e9898ec7dd5b5b9e5ceeb84497bed8
datnt-0249 <nguyen.tien.dat-d@sun-asterisk.com>
2024-07-25 15:32:57 +0700

    init db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Post.destroy_all
+
+10.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+users = User.order(:created_at).take(6)
+
+30.times do |i|
+  title = Faker::Lorem.sentence(word_count: 10)
+  content = Faker::Lorem.sentence(word_count: 50)
+  users.each{|user| user.posts.create!(title: title, content: content, status: [0, 1].sample)}
+end
