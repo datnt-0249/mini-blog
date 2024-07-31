d24445f38a40f3b83d691eaaff928e16abd6f50d
datnt-0249 <nguyen.tien.dat-d@sun-asterisk.com>
2024-07-31 14:52:58 +0700

    Edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, :correct_user, only: %i(edit update destroy)
   before_action :logged_in_user, except: %i(index)
 
   def index
@@ -21,8 +22,41 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @post.update post_params
+      flash[:success] = t ".success"
+      redirect_to @post.user
+    else
+      flash.now[:failed] = t ".failed"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:success] = t ".success"
+    else
+      flash[:failed] = t ".failed"
+    end
+    redirect_to @post.user
+  end
+
   private
   def post_params
     params.require(:post).permit Post::UPDATABLE_ATTRS
+  end
+
+  def find_post
+    @post = Post.find_by id: params[:id]
+    return if @post
+
+    flash[:warning] = t ".post_not_found"
+    redirect_to root_path
+  end
+
+  def correct_user
+    redirect_to root_path, status: :see_other unless current_user? @post.user
   end
 end
