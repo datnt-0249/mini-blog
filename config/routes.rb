cf6c2345cf3412f3d77dab272392740a5a393520
datnt-0249 <nguyen.tien.dat-d@sun-asterisk.com>
2024-08-02 10:23:41 +0700

    Following post

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    get "/following-posts", to: "posts#following_posts"
     resources :users
     resources :posts do
       resources :likes, only: %i(create destroy)
